ec8bfafed00ac67fcca9b2e0014304244a973214
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2021-09-12 12:07:01 +0500

    done

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -2,40 +2,42 @@ import 'package:flutter/material.dart';
 import 'task.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class TaskData extends ChangeNotifier{
+class TaskData extends ChangeNotifier {
+  SharedPreferences prefs;
+
   void loadData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs = await SharedPreferences.getInstance();
     final keys = prefs.getKeys();
-    for(String key in keys){
+    for (String key in keys) {
       addTask(prefs.get(key));
     }
   }
 
-  TaskData(){
+  TaskData() {
     loadData();
   }
 
   List<Task> tasks = [];
 
-  int get taskCount{
+  int get taskCount {
     return tasks.length;
   }
 
   void addTask(String newTaskTitle) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs = await SharedPreferences.getInstance();
     prefs.setString(newTaskTitle, newTaskTitle);
     final task = Task(name: newTaskTitle);
     tasks.add(task);
     notifyListeners();
   }
 
-  void updateTask(Task task){
+  void updateTask(Task task) {
     task.toggleDone();
     notifyListeners();
   }
 
   void deleteTask(Task task) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs = await SharedPreferences.getInstance();
     prefs.remove(task.name);
     tasks.remove(task);
     notifyListeners();
